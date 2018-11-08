2ad52ced6edb1e20293ee4c74a3d39599baa3eb1
Ndifreke Ekim <ndyekim@gmail.com>
2018-11-08 22:01:53 +0100

    Include Mocha test folder

---
+++ test/api_test.js
@@ -0,0 +1,10 @@
+let assert = require("assert");
+
+
+describe("API TEST", function(){
+ describe("Hello world", function(){
+  it("should return two adding 1 + 10",function(){
+   assert.equal(1+1,2);
+  })
+ })
+})
