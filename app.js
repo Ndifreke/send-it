5c9f4ff198a266efd68f11e447423bf411f4f52c
Ndifreke Ekim <ndyekim@gmail.com>
2018-11-24 19:13:01 +0100

    Implement jwt token

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ import bodyParser from 'body-parser';
 import db from './server/module/Database';
 
 import view from './server/module/views';
+<<<<<<< Updated upstream
+=======
+import {
+  authenticateRoute
+} from './server/module/authenticate';
+>>>>>>> Stashed changes
 
 
 import {
@@ -16,8 +22,8 @@ import {
   updateStatus,
   changeCordinate
 }
-  from './server/controller/parcel';
- 
+from './server/controller/parcel';
+
 import {
   signup,
   login,
@@ -27,35 +33,57 @@ import {
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'ui/pages'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'ui')));
-app.use(bodyParser());
+app.set( 'views', path.join( __dirname, 'ui/pages' ) );
+app.set( 'view engine', 'ejs' );
+app.use( express.static( path.join( __dirname, 'ui' ) ) );
+app.use( bodyParser() );
 
+<<<<<<< Updated upstream
 app.post('/api/v1/parcels', createParcel);
 app.get('/api/v1/parcels', getAllParcels);
 app.get('/api/v1/parcels/:id', getOneParcel);
 app.put('/api/v1/parcels/:id', cancelParcel);
 app.use('/api/v1/users/:id/parcels', getUserParcels);
+=======
+app.post( '/api/v1/parcels', createParcel );
+app.get( '/api/v1/parcels', getAllParcels );
+app.get( '/api/v1/parcels/:id', getOneParcel );
+app.put( '/api/v1/parcels/:id', cancelParcel );
+app.use( '/api/v1/users/:id/parcels', getUserParcels );
+>>>>>>> Stashed changes
 
-app.get('/', view.renderHome);
+app.get( '/', view.renderHome );
 
+<<<<<<< Updated upstream
 app.post('/api/v1/auth/signup', signup);
 app.post('/api/v1/auth/login', login);
 app.put('/api/v1/settings', changeSettings);
 app.put('/api/v1/parcels/:id/status', updateStatus);
 app.put('/api/v1/parcels/:id/destination', changeCordinate);
 app.put('/api/v1/parcels/:id/presentLocation', changePresentLocation);
+=======
+app.post( '/api/v1/auth/signup', signup );
+app.post( '/api/v1/auth/login', login );
+app.put( '/api/v1/parcels/:id/status', updateStatus );
+app.put( '/api/v1/parcels/:id/destination', changeCordinate );
+app.put( '/api/v1/parcels/:id/presentLocation', changePresentLocation );
+>>>>>>> Stashed changes
 
-app.use((req, res) => {
-  console.log(req.url)
+app.use( ( req, res ) => {
+  console.log( req.url )
   res.statusCode = 500;
   const msg = {
     status: 'error',
     message: 'we dont quiit understand your request at this time. Project is WIP',
   };
-  res.end(JSON.stringify(msg), null, '\t');
-});
+  res.end( JSON.stringify( msg ), null, '\t' );
+} );
 
 
+<<<<<<< Updated upstream
 app.listen(process.env.PORT);
+=======
+app.listen( process.env.PORT );
+
+export default app;
+>>>>>>> Stashed changes
