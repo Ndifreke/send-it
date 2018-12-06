34f59722267fed2f630b15ce8b529b4a9dc49ebd
Ndifreke Ekim <ndyekim@gmail.com>
2018-12-06 16:00:49 +0100

    Update babel dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import view from './server/module/views';
 
 import {
   cancelParcel,
@@ -22,43 +23,41 @@ import {
 
 const app = express();
 
-app.set( 'views', path.join( __dirname, 'ui/pages' ) );
-app.set( 'view engine', 'ejs' );
-app.use( express.static( path.join( __dirname, 'ui' ) ) );
-app.use( bodyParser() );
+app.set('views', path.join(__dirname, 'ui/pages'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'ui')));
+app.use(bodyParser());
 
 
 app.use('/api/v1/users/:id/parcels', getUserParcels);
 
-app.post( '/api/v1/parcels', createParcel );
-app.get( '/api/v1/parcels', getAllParcels );
-app.get( '/api/v1/parcels/:id', getOneParcel );
-app.put( '/api/v1/parcels/:id', cancelParcel );
-app.use( '/api/v1/users/:id/parcels', getUserParcels );
+app.post('/api/v1/parcels', createParcel);
+app.get('/api/v1/parcels', getAllParcels);
+app.get('/api/v1/parcels/:id', getOneParcel);
+app.put('/api/v1/parcels/:id', cancelParcel);
+app.use('/api/v1/users/:id/parcels', getUserParcels);
 
-app.use('/api/v1/users/:id/parcels', getUserParcels); 
+app.use('/api/v1/users/:id/parcels', getUserParcels);
 
 
-app.get( '/', view.renderHome );
+app.get('/', view.renderHome);
 
-app.post( '/api/v1/auth/signup', signup );
-app.post( '/api/v1/auth/login', login );
-app.put( '/api/v1/parcels/:id/status', updateStatus );
-app.put( '/api/v1/parcels/:id/destination', changeCordinate );
-app.put( '/api/v1/parcels/:id/presentLocation', changePresentLocation );
+app.post('/api/v1/auth/signup', signup);
+app.post('/api/v1/auth/login', login);
+app.put('/api/v1/parcels/:id/status', updateStatus);
+app.put('/api/v1/parcels/:id/destination', changeCordinate);
+app.put('/api/v1/parcels/:id/presentLocation', changePresentLocation);
 
-app.use( ( req, res ) => {
-  console.log( req.url )
+app.use((req, res) => {
+  console.log(req.url)
   res.statusCode = 500;
   const msg = {
     status: 'error',
     message: 'we dont quiit understand your request at this time. Project is WIP',
   };
-  res.end( JSON.stringify( msg ), null, '\t' );
-} );
+  res.end(JSON.stringify(msg), null, '\t');
+});
 
 
 app.listen(process.env.PORT);
 export default app;
-
-
